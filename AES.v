304ae581bac7c6ffa3ba33792c9933e95c3e049f
yuyu0830 <yuyu0830@naver.com>
2024-01-05 10:59:10 +0900

    Fix : 이름 수정

--- AES.v
+++ AES.v
@@ -1,4 +1,4 @@
-module AES(
+module AES(Y
     input i_Clk, i_Rst,
     input i_fStart,
     input i_fEnc,
@@ -10,7 +10,7 @@ module AES(
     // parameter
     parameter
         IDLE    = 3'b000,
-        INV_KEY = 3'b001,
+        GEN_KEY = 3'b001,
         FIRST   = 3'b010,
         REPEAT  = 3'b011,
         FINAL   = 3'b100,
@@ -25,7 +25,7 @@ module AES(
     reg [3:0] c_Round, n_Round;
 
     reg [127:0] c_Key, n_Key,
-                c_RoundText, n_RoundText,
+                c_Text, n_Text,
                 c_NextKey, n_NextKey;
 
 
@@ -42,11 +42,11 @@ module AES(
 
     wire fIsFirstText;
     wire fLstRepeat;
-    wire fIdle, fFirst, fInvKey, fRepeat, fFinal;
+    wire fIdle, fFirst, fGenKey, fRepeat, fFinal;
 
 
     // Module
-    SubByte      SB(c_RoundText, SB_o_Data, i_fEnc);
+    SubByte      SB(c_Text, SB_o_Data, i_fEnc);
     ShiftRow     SR(SB_o_Data, SR_o_Data, i_fEnc);
     MixCol_Top   MC0(MC_i_Data, MC_o_Data, i_fEnc);
 
@@ -56,13 +56,13 @@ module AES(
     // assign
     assign o_fDone = c_State == DONE;
 
-    assign o_Data = o_fDone ? c_RoundText : 0;
+    assign o_Data = o_fDone ? c_Text : 0;
 
     assign fLstRepeat = c_Round == ROUND_MAX;
 
     assign fIsFirstText = i_fStart & c_FirstText;
 
-    assign fKE_Encrypt = fInvKey | i_fEnc;
+    assign fKE_Encrypt = fGenKey | i_fEnc;
 
     assign fKeyMC = fRepeat & !i_fEnc;
 
@@ -70,7 +70,7 @@ module AES(
 
     assign
         fIdle   = c_State == IDLE,
-        fInvKey = c_State == INV_KEY,
+        fGenKey = c_State == GEN_KEY,
         fFirst  = c_State == FIRST,
         fRepeat = c_State == REPEAT,
         fFinal  = c_State == FINAL;
@@ -80,14 +80,14 @@ module AES(
             c_State     = IDLE;
             c_Round     = 0;
             c_Key       = 0;
-            c_RoundText = 0;
+            c_Text      = 0;
             c_FirstText = 1;
             c_NextKey   = 0;
         end else begin
             c_State     = n_State;
             c_Round     = n_Round;
             c_Key       = n_Key;
-            c_RoundText = n_RoundText;
+            c_Text      = n_Text;
             c_FirstText = n_FirstText;
             c_NextKey   = n_NextKey;
         end
@@ -95,11 +95,11 @@ module AES(
 
     always@*
     begin
-        n_RoundText = 
+        n_Text = 
             fIdle   ? 0 :
             fFirst  ? i_Text ^ c_NextKey :
             fRepeat ? (fKeyMC ? MC_o_Data : MC_o_Data ^ c_Key) :
-            fFinal  ? SR_o_Data ^ c_Key : c_RoundText;
+            fFinal  ? SR_o_Data ^ c_Key : c_Text;
 
         n_Key = 
             fIsFirstText ? i_Key :
@@ -107,18 +107,18 @@ module AES(
             
         n_FirstText = fFirst ? 0 : c_FirstText;
 
-        n_Round = fFirst ? 1 : (fRepeat | fInvKey ? (fLstRepeat ? 0 : c_Round + 1) : 0);
+        n_Round = fFirst ? 1 : (fRepeat | fGenKey ? (fLstRepeat ? 0 : c_Round + 1) : 0);
         
         n_NextKey = 
             fIsFirstText ? i_Key :
             i_fEnc & fFinal ? c_Key :
-            fLstRepeat & fInvKey ? KE_o_Key : c_NextKey;
+            fLstRepeat & fGenKey ? KE_o_Key : c_NextKey;
 
         n_State = c_State;
         case(c_State)
-            IDLE:    n_State = i_fStart ? (i_fEnc ? FIRST : INV_KEY) : c_State;
+            IDLE:    n_State = i_fStart ? (i_fEnc ? FIRST : GEN_KEY) : c_State;
 
-            INV_KEY: n_State = fLstRepeat ? FIRST : c_State;
+            GEN_KEY: n_State = fLstRepeat ? FIRST : c_State;
 
             FIRST:   n_State = REPEAT;
 
